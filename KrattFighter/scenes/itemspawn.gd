293aa800e947c750521a47f5959461841ae013bf
Gregor Artur <144439475+greeeegartur@users.noreply.github.com>
2025-01-25 21:41:08 +0200

    map muutused

--- KrattFighter/scenes/itemspawn.gd
+++ KrattFighter/scenes/itemspawn.gd
@@ -1,26 +1,17 @@
 extends Node2D
+@onready var textures = []
+@onready var spawns = [$"Spawns (11 kokku)/mets1/Node2D", $"Spawns (11 kokku)/peenar1/Node2D", $"Spawns (11 kokku)/koobas2/Node2D", $"Spawns (11 kokku)/koobas1/Node2D", $"Spawns (11 kokku)/maja2/Node2D", $"Spawns (11 kokku)/maja1/Node2D", $"Spawns (11 kokku)/küngas1/Node2D", $"Spawns (11 kokku)/järv2/Node2D", $"Spawns (11 kokku)/järv1/Node2D", $"Spawns (11 kokku)/peenar2/Node2D", $"Spawns (11 kokku)/küngas2/Node2D"]
 
-@onready var itemTextures = {"ämber": "res://assets/drive-download-20250125T091820Z-001/amber.png",
- "armor": "res://assets/drive-download-20250125T091820Z-001/armor.png",
- "axe": "res://assets/drive-download-20250125T091820Z-001/axe.png",
- "hat": "res://assets/drive-download-20250125T091820Z-001/hat.png",
- "kivi": "res://assets/drive-download-20250125T091820Z-001/kivi.png",
- "vikat": "res://assets/drive-download-20250125T091820Z-001/kosa.png",
- "kott": "res://assets/drive-download-20250125T091820Z-001/meshok.png",
- "mõõk": "res://assets/drive-download-20250125T091820Z-001/mook.png",
- "pitchfork": "res://assets/drive-download-20250125T091820Z-001/pitchfork.png",
- "plough": "res://assets/drive-download-20250125T091820Z-001/plug.png",
- "pot": "res://assets/drive-download-20250125T091820Z-001/pot.png",
- "shirt": "res://assets/drive-download-20250125T091820Z-001/shirt2.png",
- "hoe": "res://assets/drive-download-20250125T091820Z-001/smth.png",
- "stick": "res://assets/drive-download-20250125T091820Z-001/stiik.png",
- "millstone": "res://assets/drive-download-20250125T091820Z-001/stone.png",
- "tõllupea": "res://assets/drive-download-20250125T091820Z-001/suur-t]ll.png",
- "saba" :"res://assets/drive-download-20250125T091820Z-001/tail.png",
- "kuju": "res://assets/drive-download-20250125T091820Z-001/totem.png"
-}
-
-@onready var metsaSpawns = [ItemManager.ITEMS.AMBER, ItemManager.ITEMS.AXE]
+func getTextures():
+	for i in range(ItemManager.items_dict.size()):
+		if ItemManager.items_dict[i].default:
+			print("get ignored")
+		else:
+			textures.append(ItemManager.items_dict[i].texture)
 
 func _ready():
-	$"Spawns (11 kokku)/järv2/Sprite2D".texture = ItemManager.items_dict[ItemManager.ITEMS.SWORD].texture
+	getTextures()
+	print(textures)
+	for i in range(spawns.size()):
+		var randomnessFactor = (randi() % textures.size() - 1)
+		spawns[i].icon = textures[randomnessFactor]
