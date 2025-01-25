25c028058292b7a98745c3a4d89770d3c70e37dc
KaarenTenson <tensonkaaren@gmail.com>
2025-01-25 18:56:46 +0200

    build scene

--- KrattFighter/scripts/inventory_slot.gd
+++ KrattFighter/scripts/inventory_slot.gd
@@ -1,16 +1,20 @@
 extends Button
-
-var mousePos
-var buttonPressed
-var iconTexture
-@export var stats : Items = null:
-	set(value):
-		stats = value
-		
-		if value != null:
-			icon = value.texture
-		else:
-			icon = null
-
+class_name InventorySlot
+signal clicked(btn:InventorySlot)
+var in_air:bool=false
+var item:Items
+var margin:=self.get_rect().size.x/2
+func set_item(item:Items):
+	self.item=item
+	self.icon=item.texture
 func _ready():
+	pass
+func _process(delta: float) -> void:
+	if(in_air):
+		self.global_position=get_global_mouse_position()+Vector2(-margin, -margin)
+func _on_pressed() -> void:
+	in_air=true
 	set_process_input(false)
+	clicked.emit(self)
+func free_mouse():
+	in_air=false
