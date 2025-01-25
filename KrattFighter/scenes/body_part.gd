c45ca19290e2f2f25220c9c264fbdf0db2975ed7
KaarenTenson <tensonkaaren@gmail.com>
2025-01-25 13:58:22 +0200

    player scene

--- KrattFighter/scenes/body_part.gd
+++ KrattFighter/scenes/body_part.gd
@@ -1,20 +1,11 @@
-extends Node2D
-signal start
+extends StaticBody2D
+@export var body_part:String
 
-const PLAYER = preload("res://scenes/player.tscn")
-const ENEMY=preload("res://scenes/Enemy.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start.emit()
-	
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func load_player():
-	pass
-func load_enemy():
 	pass
---
+++ KrattFighter/scripts/player.gd
@@ -0,0 +1,17 @@
+extends Node2D
+@onready var head: StaticBody2D = $Head
+@onready var chest: StaticBody2D = $chest
+@onready var left_hand: StaticBody2D = $LeftHand
+@onready var right_hand: StaticBody2D = $rightHand
+@onready var left_leg: StaticBody2D = $leftLeg
+@onready var rightleg: StaticBody2D = $rightleg
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
