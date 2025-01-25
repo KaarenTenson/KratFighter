a109ebd4f22b858eef314cc84b3b0ee0b8725005
KaarenTenson <tensonkaaren@gmail.com>
2025-01-25 22:18:50 +0200

    inventory

--- KrattFighter/scenes/equipment_panel.gd
+++ KrattFighter/scenes/equipment_panel.gd
@@ -7,15 +7,7 @@ class_name EquipmentPanel
 @onready var l_leg_panel: Panel = $EquipmentContainer/LegMargin/HBoxContainer/LHandMargin/L_LegPanel
 @onready var r_leg_panel: Panel = $EquipmentContainer/LegMargin/HBoxContainer/MarginContainer/RLegPanel
 
-var btn_dict:Dictionary={
-	head_panel: null,
-	body_panel: null,
-	l_hand_panel:null,
-	r_hand_panel:null,
-	l_leg_panel:null,
-	r_leg_panel:null,
-	
-}
+var btn_dict:Dictionary
 var translate_enum_dict:Dictionary
 var active_panel:Panel
 
@@ -27,6 +19,15 @@ func _ready() -> void:
 	ItemManager.BODY_PART.HEAD: [head_panel],
 	ItemManager.BODY_PART.CHEST: [body_panel]
 }
+	btn_dict={
+		head_panel: null,
+		body_panel: null,
+		l_hand_panel:null,
+		r_hand_panel:null,
+		l_leg_panel:null,
+		r_leg_panel:null,
+		
+	}
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -58,17 +59,23 @@ func is_equpping(body_part:int):
 		ItemManager.BODY_PART.CHEST:
 			return is_inside(body_panel)
 func is_inside(panel:Panel)->bool:
-	var panel_size:Vector2=panel.size
+	var panel_size:Vector2=panel.get_global_rect().size
 	var mouse_pos:Vector2=get_global_mouse_position()
-	var panel_pos:Vector2=panel.get_global_mouse_position()
+	var panel_pos:Vector2=panel.get_global_rect().position
 	if (mouse_pos.x > panel_pos.x and 
 	mouse_pos.x < panel_pos.x + panel_size.x and 
 	mouse_pos.y > panel_pos.y and 
-	panel_pos.y < panel_pos.y +panel_size.y):
+	panel_pos.y < panel_pos.y + panel_size.y):
 		active_panel=panel
 		return true
 	return false
 func add_item(button:InventorySlot):
-	active_panel.add_child(button)
+	btn_dict[active_panel]=button
+	button.reparent(active_panel)
 	
 	
+	
+func _on_inventory_container_remove_btn(btn: InventorySlot) -> void:
+	for key in btn_dict.keys():
+		if(btn_dict[key]==btn):
+			btn_dict[key]=null
--- KrattFighter/scenes/inventory_container.gd
+++ KrattFighter/scenes/inventory_container.gd
@@ -6,13 +6,14 @@ var cur_button:InventorySlot=null
 
 signal high_light(body_part:int)
 signal un_high_light(body_part:int)
+signal remove_btn(btn:InventorySlot)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	inv_instance=INVENTORY_SLOT.instantiate()
 	for item in ItemManager.current_items:
 		var new_obj :=inv_instance.duplicate()
 		find_empty().add_child(new_obj)
-		new_obj.clicked.connect(handle_btn_input)
+		new_obj.pressed.connect(handle_btn_input.bind(new_obj))
 		new_obj.set_item(ItemManager.items_dict[item])
 		new_obj.set_position(Vector2(0,0))
 
@@ -27,6 +28,8 @@ func _process(delta: float) -> void:
 func handle_btn_input(btn:InventorySlot):
 	if(cur_button!=null):
 		return
+	btn.top_level=true
+	btn.in_air=true
 	high_light.emit(btn.item.body_part)
 	await get_tree().create_timer(0.2).timeout
 	cur_button=btn
@@ -34,10 +37,16 @@ func handle_btn_input(btn:InventorySlot):
 func _on_input_manager_global_clicked() -> void:
 	if(cur_button)==null:
 		return
+	var is_in_invetory=false
+	if(equipment_panel.btn_dict.values().has(cur_button)):
+		is_in_invetory=true
 	if(equipment_panel.is_equpping(cur_button.item.body_part)):
-		equipment_panel.add_item(cur_button)
+		if(!is_in_invetory):
+			equipment_panel.add_item(cur_button)
+	if(is_in_invetory):
+		remove_btn.emit(cur_button)
+		cur_button.reparent(find_empty())
 	un_high_light.emit(cur_button.item.body_part)
 	cur_button.free_mouse()
 	await  get_tree().create_timer(0.2).timeout
 	cur_button=null
-	
